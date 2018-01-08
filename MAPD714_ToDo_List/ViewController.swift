f9a880158729b603f2bc94e782e0f29c24e919d0
SergioBrunacci <sergio.a.brunacci@gmail.com>
2018-01-08 15:16:46 -0500

    Initial Commit

---
+++ MAPD714_ToDo_List/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MAPD714_ToDo_List
+//
+//  Created by Sergio de Almeida Brunacci on 2018-01-08.
+//  Copyright © 2018 Centennial College. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
