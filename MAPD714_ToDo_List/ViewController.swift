b10f9f1fe4d3b408105a4494a15c545d5fd59d16
SergioBrunacci <sergio.a.brunacci@gmail.com>
2018-01-08 16:26:20 -0500

    Implement add item

--- MAPD714_ToDo_List/ViewController.swift
+++ MAPD714_ToDo_List/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.title = "To-Do List"
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(ViewController.didTapAddItemButton(_:)))
     }
 
     override func didReceiveMemoryWarning() {
@@ -44,7 +47,54 @@ class ViewController: UITableViewController {
         
         return cell
     }
-
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+    {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        if indexPath.row < todoItems.count
+        {
+            let item = todoItems[indexPath.row]
+            item.done = !item.done
+            
+            tableView.reloadRows(at: [indexPath], with: .automatic)
+        }
+    }
+    
+    @objc func didTapAddItemButton(_ sender:UIBarButtonItem)
+    {
+        //Create Alert
+        let alert = UIAlertController(title: "New to-do item", message: "Insert the title of the new to-do item", preferredStyle: .alert)
+        
+        //Add text field for the new item
+        alert.addTextField(configurationHandler: nil)
+        
+        //Cancel button
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        //OK button
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (_) in
+            if let title = alert.textFields?[0].text
+            {
+                self.addNewToDoItem(title: title)
+            }
+        }))
+        
+        // Present alert
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    private func addNewToDoItem(title: String)
+    {
+        //Index
+        let newIndex = todoItems.count
+        
+        //New item
+        todoItems.append(ToDoItem(title: title))
+        
+        //Tell the table view
+        tableView.insertRows(at: [IndexPath(row: newIndex, section: 0)], with: .top)
+    }
 
 }
 
